bc1e7afdd726fe480893c82c8aa9fa325bc55148
JeongJoonLee-kr <110399451+JeongJoonLee-kr@users.noreply.github.com>
2024-11-20 23:31:49 +1100

    Time: 2169 ms (5%), Space: 16.5 MB (88.89%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,8 +1,8 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x == 0:
-            return x
         for i in range(x+1):
-            if i**2 > x:
+            if i**2 == x:
+                return i
+            elif i**2 > x:
                 return i-1
         
